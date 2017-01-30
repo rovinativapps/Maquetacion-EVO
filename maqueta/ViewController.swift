3abb9bf25326c6c67a23580a4c8a7463a89b28bc
mroncalloNativapps <mroncallo@nativapps.mobi>
2017-01-30 14:38:12 -0500

    Initial Commit

---
+++ maqueta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  maqueta
+//
+//  Created by Miguel Roncallo on 1/30/17.
+//  Copyright © 2017 Nativapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
